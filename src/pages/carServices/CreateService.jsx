c1a718d6b90da70ebe13160e50d7c8169f1969af
bautibadino <bautistabadino@gmail.com>
2023-09-23 00:37:33 +0200

    advances

--- src/pages/carServices/CreateService.jsx
+++ src/pages/carServices/CreateService.jsx
@@ -17,6 +17,58 @@ export const CreateService = ({ services }) => {
   const [suggestions, setSuggestions] = useState([]);
   const [selectedSuggestion, setSelectedSuggestion] = useState("");
   const [selectedService, setSelectedService] = useState(null);
+
+  //estados de inputs
+  const [vehicleBrand , setVehicleBrand] = useState("")
+  const [vehicleModel , setVehicleModel] = useState("")
+  const [vehicleYear , setVehicleYear] = useState("")
+  const [km, setKm] = useState("")
+  const [ownerName , setOwnerName] = useState("")
+  const [ownerEmail , setOwnerEmail] = useState("")
+  const [ownerPhone , setOwnerPhone] = useState("")
+  const [motorOilType , setMotorOilType] = useState("")
+  const [motorOilChange , setMotorOilChange] = useState(false)
+  const [steeringOilType , setSteeringOilType] = useState("")
+  const [steeringOilChange , setSteeringOilChange] = useState(false)
+  const [steeringOilReviewed, setSteeringOilReviewed] = useState(false)
+  const [gearboxOilType , setGearboxOilType] = useState("")
+  const [gearboxOilChange , setGearboxOilChange] = useState(false)
+  const [gearboxOilReviewed, setGearboxOilReviewed] = useState(false)
+  const [airFilterChange , setAirFilterChange] = useState(false)
+  const [airFilterReviewed, setAirFilterReviewed] = useState(false)
+  const [oilFilterChange , setOilFilterChange] = useState(false)
+  const [fuelFilterChange, setFuelFilterChange] = useState(false)
+  const [patent , setPatent] = useState("")
+  const [date, setDate] = useState(formattedDate)
+  //fin estados
+
+  //crear service 
+  const createService = async () => {
+    const response = await fetch("http://localhost:4000/api/services", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        Date: date,
+        vehicle: {
+          brand: vehicleBrand,
+          model: vehicleModel,
+          year: vehicleYear,
+        },
+        km: km,
+        owner: {
+          ownerName: ownerName,
+          email: ownerEmail,
+        },
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+  };
+
+
+
   const handleSearch = (e) => {
     const newValue = e.target.value;
     setSearch(newValue);
@@ -61,10 +113,8 @@ export const CreateService = ({ services }) => {
 
   useEffect(() => {
     if (search.startsWith(previousSearch)) {
-      // Si la búsqueda actual comienza con la búsqueda anterior, solo filtramos los resultados actuales.
       filterServicesByPatent();
     } else {
-      // Si la búsqueda actual no comienza con la búsqueda anterior, filtramos desde el conjunto completo de servicios.
       setFilteredServices([]);
       setPreviousSearch(search);
     }
@@ -72,15 +122,10 @@ export const CreateService = ({ services }) => {
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
-
-    //al dar click buscar el service por la pantente y mostrarlo en el formulario
-    search.includes(" ") ? alert("no se puede buscar con espacios") : null;
-
     const service = services.find((service) => service.patent === search);
     setSelectedService(service);
   };
-  console.log(selectedService);
-
+  console.log(selectedService)
   return (
     <div className="w-full rounded-lg bg-white px-[24px] py-[20px] dark:bg-darkblack-600 ">
       <div className="flex flex-row w-full ">
@@ -186,82 +231,21 @@ export const CreateService = ({ services }) => {
               </div>
             </div>
           </div>
-          <div className="flex flex-col justify-start items-start">
-            <div className="flex flex-col w-full items-center mt-10">
-              <div className="flex flex-col w-1/4 mx-3">
-                <label htmlFor="patent" className="dark:text-white">Aceite motor</label>
-                <div className="flex flex-row">
-                  <input
-                    className="w-full border-none bg-slate-100 rounded-md dark:text-darkblack-600"
-                    name="motorOil"
-                  />
-                  <select className="border-none bg-slate-100 rounded-md mx-2 dark:text-darkblack-600">
-                    <option className="" value="true">
-                      Si
-                    </option>
-                    <option className="" value="false">
-                      No
-                    </option>
-                  </select>
-                </div>
-              </div>
-              <div className="flex flex-col w-1/4 mx-3">
-                <label htmlFor="patent" className="dark:text-white">Aceite caja</label>
-                <div className="flex flex-row">
-                  <input
-                    className="w-full border-none bg-slate-100 rounded-md dark:text-darkblack-600"
-                    name="gearBoxOil"
-                  />
-                  <select className="border-none bg-slate-100 rounded-md mx-2 dark:text-darkblack-600">
-                    <option className="" value="true">
-                      Si
-                    </option>
-                    <option className="" value="false">
-                      No
-                    </option>
-                  </select>
-                </div>
-              </div>
-              <div className="flex flex-col w-1/4 mx-3">
-                <label htmlFor="patent" className="dark:text-white">Aceite direccion</label>
-                <div className="flex flex-row">
-                  <input
-                    className="w-full border-none bg-slate-100 rounded-md dark:text-darkblack-600"
-                    name="steeringOil"
-                  />
-                  <select className="border-none bg-slate-100 rounded-md mx-2 dark:text-darkblack-600">
-                    <option className="" value="true">
-                      Si
-                    </option>
-                    <option className="" value="false">
-                      No
-                    </option>
-                  </select>
-                </div>
-              </div>
-              <div className="flex flex-row mt-6 m-3 w-1/3">
+          
+          <div className="grid grid-cols-2 ">
+            <div className="flex flex-col w-full items-center justify-center rounded-md col-start-1 col-end-2">
+            <div className="flex flex-row mt-4 m-3 ">
                   <div className="flex flex-col">
-                    <label htmlFor="patent" className="dark:text-white">Filtro de aire</label>
+                    <label htmlFor="patent" className="dark:text-white">Aceite motor</label>
                     <div className="flex flex-row">
                       <input
                         className="w-full border-none bg-slate-100 rounded-md dark:text-darkblack-600"
-                        name="airFilter"
+                        name="motorOil"
                       />
                     </div>
                   </div>
                   <div className="flex flex-col">
-                      <label className="">cambio</label>
-                      <select className="border-none bg-slate-100 rounded-md mx-2 dark:text-darkblack-600">
-                        <option className="" value="true">
-                          Si
-                        </option>
-                        <option className="" value="false">
-                          No
-                        </option>
-                      </select>
-                    </div>
-                    <div className="flex flex-col">
-                      <label className="">revisado</label>
+                      <label className="text-center">cambio</label>
                       <select className="border-none bg-slate-100 rounded-md mx-2 dark:text-darkblack-600">
                         <option className="" value="true">
                           Si
@@ -272,18 +256,18 @@ export const CreateService = ({ services }) => {
                       </select>
                     </div>
               </div>
-              <div className="flex flex-row mt-6 m-3 w-1/4">
+              <div className="flex flex-row mt-4 m-3 ">
                   <div className="flex flex-col">
-                    <label htmlFor="patent" className="dark:text-white">Filtro de aceite</label>
+                    <label htmlFor="patent" className="dark:text-white">Aceite de dirección</label>
                     <div className="flex flex-row">
                       <input
                         className="w-full border-none bg-slate-100 rounded-md dark:text-darkblack-600"
-                        name="airFilter"
+                        name="steeringOil"
                       />
                     </div>
                   </div>
                   <div className="flex flex-col">
-                      <label className="">cambio</label>
+                      <label className="text-center">cambio</label>
                       <select className="border-none bg-slate-100 rounded-md mx-2 dark:text-darkblack-600">
                         <option className="" value="true">
                           Si
@@ -294,18 +278,18 @@ export const CreateService = ({ services }) => {
                       </select>
                     </div>
               </div>
-              <div className="flex flex-row mt-6 m-3 w-1/3">
+              <div className="flex flex-row mt-4 m-3 ">
                   <div className="flex flex-col">
-                    <label htmlFor="patent" className="dark:text-white">Filtro de combustible</label>
+                    <label htmlFor="patent" className="dark:text-white">Aceite caja</label>
                     <div className="flex flex-row">
                       <input
                         className="w-full border-none bg-slate-100 rounded-md dark:text-darkblack-600"
-                        name="airFilter"
+                        name="gearboxOil"
                       />
                     </div>
                   </div>
                   <div className="flex flex-col">
-                      <label className="">cambio</label>
+                      <label className="text-center">cambio</label>
                       <select className="border-none bg-slate-100 rounded-md mx-2 dark:text-darkblack-600">
                         <option className="" value="true">
                           Si
@@ -317,6 +301,81 @@ export const CreateService = ({ services }) => {
                     </div>
               </div>
             </div>
+            
+            <div className="flex flex-col w-full items-center justify-center rounded-md col-start-2 col-end-">
+            <div className="flex flex-row mt-4 m-3">
+                  <div className="flex flex-col">
+                    <label htmlFor="patent" className="dark:text-white">Filtro de aire</label>
+                    <div className="flex flex-row">
+                      <input
+                        className="w-full border-none bg-slate-100 rounded-md dark:text-darkblack-600"
+                        name="airFilter"
+                      />
+                    </div>
+                  </div>
+                  <div className="flex flex-col">
+                      <label className="text-center">cambio</label>
+                      <select className="border-none bg-slate-100 rounded-md mx-2 dark:text-darkblack-600">
+                        <option className="" value="true">
+                          Si
+                        </option>
+                        <option className="" value="false">
+                          No
+                        </option>
+                      </select>
+                    </div>
+              </div>
+              <div className="flex flex-row mt-4 m-3">
+                  <div className="flex flex-col">
+                    <label htmlFor="patent" className="dark:text-white">Filtro de aceite</label>
+                    <div className="flex flex-row">
+                      <input
+                        className="w-full border-none bg-slate-100 rounded-md dark:text-darkblack-600"
+                        name="oilFilter"
+                      />
+                    </div>
+                  </div>
+                  <div className="flex flex-col">
+                      <label className="text-center">cambio</label>
+                      <select className="border-none bg-slate-100 rounded-md mx-2 dark:text-darkblack-600">
+                        <option className="" value="true">
+                          Si
+                        </option>
+                        <option className="" value="false">
+                          No
+                        </option>
+                      </select>
+                    </div>
+              </div>
+              <div className="flex flex-row mt-4 m-3">
+                  <div className="flex flex-col">
+                    <label htmlFor="patent" className="dark:text-white">Filtro de combustible</label>
+                    <div className="flex flex-row">
+                      <input
+                        className="w-full border-none bg-slate-100 rounded-md dark:text-darkblack-600"
+                        name="fuelFilter"
+                      />
+                    </div>
+                  </div>
+                  <div className="flex flex-col">
+                      <label className="text-center">cambio</label>
+                      <select className="border-none bg-slate-100 rounded-md mx-2 dark:text-darkblack-600">
+                        <option className="" value="true">
+                          Si
+                        </option>
+                        <option className="" value="false">
+                          No
+                        </option>
+                      </select>
+                    </div>
+              </div>
+              </div>
+          </div>
+
+          <div className="w-full flex justify-center items-center mt-16">
+            <button className="bg-yellow-400 px-4 py-2 rounded-md shadow-md active:shadow-sm">
+              Guardar
+            </button>
           </div>
         </form>
       </div>
